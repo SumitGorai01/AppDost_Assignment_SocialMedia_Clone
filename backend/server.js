3c097ea71792266de9159291fa46cc252a030d17
Sumit Gorai <goraisumit01@gmail.com>
2025-11-07 22:31:28 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: process.env.CLIENT_URL || '*' }));
 app.use(express.json({ limit: '10mb' }));
+app.use(
+  cors({
+    origin: "https://linksphere-connect.vercel.app",
+    credentials: true,
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
